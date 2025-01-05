d6e7622f9e281d98469bc3acfa2d3ac4f31a30ec
Khanh-21522203 <21522203@gm.uit.edu.vn>
2025-01-05 20:07:22 +0700

    init project

---
+++ scoring-service/src/test/java/com/JavaCSA/scoring_service/ScoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaCSA.scoring_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
